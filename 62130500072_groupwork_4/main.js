9341398b29f38e669fd0bd9af8d8ba50f79d9aa3
Pattarapol Sangsunee <Pattarapol.plasma@mail.kmutt.ac.th>
2021-02-17 12:14:27 +0700

    [UPDATE]search button

--- 62130500072_groupwork_4/main.js
+++ 62130500072_groupwork_4/main.js
@@ -1,22 +1,32 @@
 const app = {
     data() {
         return {
-            tasks: [{info: 'In the afternoon', Image: './images/cityInAfterNoon.jpg',done: false},
-                    {info: 'Empire state and the sunset', Image: './images/cityInSunset.jpg' ,done: false},
-                    {info: 'Street in small town', Image: './images/cityInNight.jpg' ,done: false}
-                ]
+            tasks: [{ info: 'In the afternoon', Image: './images/cityInAfterNoon.jpg', done: false },
+            { info: 'Empire state and the sunset', Image: './images/cityInSunset.jpg', done: false },
+            { info: 'Street in small town', Image: './images/cityInNight.jpg', done: false }
+            ],
+            search: { toggle: false, cancel: true}
         }
     },
     methods: {
-        toggleDone(index){
+        toggleDone(index) {
             this.tasks[index].done = !this.tasks[index].done
-        }
+        },
+
+        toggleicon:
+        function () {
+            this.search.toggle = !this.search.toggle
+            this.search.cancel = !this.search.cancel
+            console.log(this.search.toggle);
+        },
+
+        
     },
     computed: {
-        countUndone(){
-            return this.tasks.filter( t => !t.done ).length
+        countUndone() {
+            return this.tasks.filter(t => !t.done).length
         }
-        
+    }
 }
-}
+
 Vue.createApp(app).mount('#app')
